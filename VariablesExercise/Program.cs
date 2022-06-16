bb2c28bc0e29dffe2ff5a4b94bc02f670b22ff9f
Ernest Acosta <ernestacosta@icloud.com>
2022-06-16 16:02:11 -0700

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string petName = "Charlie";
+            int petAge = 1;
+            decimal petweight = 65.55m;
+            bool isHealthy = true;
+            char firstInitial = 'C';
+            double petHeight = 26;
+
+            Console.WriteLine($"My dog's name is {petName}. He is a {petAge} " +
+                $"year old Golden Retriever that weighs {petweight} LBS. " +
+                $"He is {petHeight} inches tall and it is {isHealthy} that he is healthy. " +
+                $"His first initial is {firstInitial}.");
+
+        }
+    }
+}
